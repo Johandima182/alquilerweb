f7ee9a86d2a34bae85be55e2fb0120ed148922dc
Johan Diaz <diazjohan21@gmail.com>
2021-11-05 16:41:29 -0500

    Creacion de Repositorios

---
+++ src/repositories/index.ts
@@ -0,0 +1,7 @@
+export * from './administrador.repository';
+export * from './asesor.repository';
+export * from './ciudad.repository';
+export * from './cliente.repository';
+export * from './departamento.repository';
+export * from './solicitud.repository';
+export * from './vehiculo.repository';
